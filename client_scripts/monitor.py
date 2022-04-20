6f0a04f6f9dacb1ec8c6ad8515a0ee386c72a97c
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-04-20 18:48:21 +0100

    adding initial script to insert data in the local database

---
+++ client_scripts/monitor.py
@@ -0,0 +1,43 @@
+import os
+import pingparser
+import psycopg2
+import datetime
+
+id_pi = 'library'
+MAX_PING = 'maxping'
+MIN_PING = 'minping'
+AVG_PING = 'avgping'
+
+os.popen('ping -c 1 192.168.1.1') # wakeup ping
+
+process = os.popen('ping -c 5 192.168.1.1')
+now = datetime.datetime.now()
+hour = now.hour
+minutes = now.minute
+seconds = now.second
+ms = now.microsecond
+
+time = f"{hour}:{minutes}:{seconds}.{ms}"
+
+output = process.read()
+results = pingparser.parse(output)
+
+max = results[MAX_PING]
+min = results[MIN_PING]
+avg = results[AVG_PING]
+
+connection = None
+
+try:
+    connection = psycopg2.connect("dbname=testdb user=pehcarmine")
+    cursor = connection.cursor()
+    data = (id_pi, max, min, avg, hour)
+    sql = f"INSERT INTO facts (id_pi, max, min, avg, hour) VALUES ('{id_pi}', {max}, {min}, {avg}, '{time}');"
+    cursor.execute(sql, data)
+    connection.commit()
+    cursor.close()
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    if connection is not None:
+        connection.close()
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import pingparser
-from collections import OrderedDict
 
 process = os.popen('ping -c 5 192.168.1.1')
 
@@ -8,8 +7,5 @@ output = process.read()
 
 results = pingparser.parse(output)
 
-# get a dictionary from the result
-ordered_dict = OrderedDict(results.items())
 
 print(results)
-print(ordered_dict['host'])
