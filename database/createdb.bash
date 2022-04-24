f2e91c2dcfc383ccfc812bcbc92b1a7abc3ec0ec
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-04-24 20:05:00 +0100

    updating bash script

--- database/createdb.bash
+++ database/createdb.bash
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
 
 createdb testdb1 -O $USER
 psql -d testdb1 -f schema.sql
 /home/pi/Repos/networking-monitoring-raspberry/.venv/bin/python /home/pi/Repos/networking-monitoring-raspberry/client_scripts/register.py
+echo -e "${GREEN}Completed."
