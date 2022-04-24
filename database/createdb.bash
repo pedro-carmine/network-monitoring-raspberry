914c3529cf43a5b55e496888cce2cf8cd27e2b24
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-04-24 20:07:16 +0100

    updating bash

--- database/createdb.bash
+++ database/createdb.bash
@@ -2,7 +2,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 
-createdb testdb1 -O $USER
-psql -d testdb1 -f schema.sql
+createdb testdb -O $USER
+psql -d testdb -f schema.sql
 /home/pi/Repos/networking-monitoring-raspberry/.venv/bin/python /home/pi/Repos/networking-monitoring-raspberry/client_scripts/register.py
 echo -e "${GREEN}Completed."
