7557c67bff42f1a86dedb6112b6149cff280d64d
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-05-13 11:45:52 +0100

    adding devices file

---
+++ monitor_website/src/pages/devices/Devices.jsx
@@ -0,0 +1,12 @@
+import "./devices.css";
+import React from "react";
+
+const Devices = () => {
+    return (
+        <div className="devices">
+            <h2 className="title">Devices List</h2>
+        </div>
+    );
+};
+
+export default Devices;
