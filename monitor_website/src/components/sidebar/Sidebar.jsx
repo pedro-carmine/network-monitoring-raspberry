544ff0956af2f54ebac2572152ea93917ef4df36
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-05-13 11:45:25 +0100

    adding devices page to react

--- monitor_website/src/components/sidebar/Sidebar.jsx
+++ monitor_website/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import "./sidebar.css";
 import { Home, Devices } from "@material-ui/icons";
+import {Link} from "react-router-dom";
 
 const Sidebar = () => {
     return (
@@ -8,14 +9,18 @@ const Sidebar = () => {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Dashboard</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem active">
-                            <Home className="sidebarIcon"/>
-                            Home
-                        </li>
-                        <li className="sidebarListItem">
-                            <Devices className="sidebarIcon" />
-                            Devices
-                        </li>
+                        <Link to="/" className="link">
+                            <li className="sidebarListItem">
+                                <Home className="sidebarIcon"/>
+                                Home
+                            </li>
+                        </Link>
+                        <Link to="/devices" className="link">
+                            <li className="sidebarListItem">
+                                <Devices className="sidebarIcon" />
+                                Devices
+                            </li>
+                        </Link>
                     </ul>
                 </div>
             </div>
