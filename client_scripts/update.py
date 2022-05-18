3aeab66aceaa3ab536f4b1b3aa65f841994ad12e
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-05-18 14:32:43 +0100

    adding raspberry pi existence verification in the update.py code to update the main database

--- client_scripts/update.py
+++ client_scripts/update.py
@@ -11,6 +11,21 @@ connection = None
 def id_date_today():
     return datetime.datetime.now().strftime('%Y%m%d')
 
+def register_pi(main_db_cursor):
+    local_connection = psycopg2.connect(f"dbname=testdb user={user}")
+    local_cursor = local_connection.cursor()
+    query = f"SELECT * FROM raspberry WHERE id_pi = '{hostname}'"
+    local_cursor.execute(query)
+    result = local_cursor.fetchall()
+    local_connection.close()
+    id_pi = result[0][0]
+    model = result[0][1]
+    location = result[0][2]
+    ip = result[0][3]
+    
+    query = f"INSERT INTO raspberry (id_pi, model, location, ip) VALUES ('{id_pi}', '{model}', '{location}', '{ip}')"
+    main_db_cursor.execute(query)
+
 def collect_all_data():
     local_conn = psycopg2.connect(f"dbname=testdb user={user}")
     cursor = local_conn.cursor()
@@ -51,9 +66,18 @@ def hour_now():
     ms = now.microsecond
     return f"{hour}:{minutes}:{seconds}.{ms}"
 
+def check_existence(cursor):
+    query = f"SELECT * FROM raspberry WHERE id_pi = '{hostname}'"
+    cursor.execute(query, hostname)
+    result = cursor.fetchall()
+    if len(result) == 0:
+        register_pi(cursor)
+
+
 try:
     connection = psycopg2.connect(f"host=192.168.1.247 dbname=testdb")
     cursor = connection.cursor()
+    check_existence(cursor)
     sql = f"SELECT * FROM last_updated WHERE id_pi = '{hostname}'"
     cursor.execute(sql, hostname)
     result = cursor.fetchall()
@@ -61,7 +85,7 @@ try:
         data_to_be_sent = collect_all_data()
         if data_to_be_sent:
             for data in data_to_be_sent:
-                sql = f"INSERT INTO facts (id_pi, id_date, max, min, avg, packets_sent, packets_received, hour) VALUES ('{data[0]}', {data[1]}, {data[2]}, {data[3]}, {data[4]}, '{data[5]}', '{data[6]}', '{data[7]}')"
+                sql = f"INSERT INTO facts (id_pi, id_date, max, min, avg, packets_sent, packets_received, packet_loss, hour) VALUES ('{data[0]}', {data[1]}, {data[2]}, {data[3]}, {data[4]}, '{data[5]}', '{data[6]}', '{data[7]}', '{data[8]}')"
                 cursor.execute(sql, data)
             time = hour_now()
             sql_last_updated = f"INSERT INTO last_updated (id_pi, hour) VALUES ('{hostname}', '{time}')"
@@ -72,7 +96,7 @@ try:
         data_to_be_sent = collect_pending_data(date, hour)
         if data_to_be_sent: # if the list is not empty
             for data in data_to_be_sent:
-                sql = f"INSERT INTO facts (id_pi, id_date, max, min, avg, packets_sent, packets_received, hour) VALUES ('{data[0]}', {data[1]}, {data[2]}, {data[3]}, {data[4]}, '{data[5]}', '{data[6]}', '{data[7]}')"
+                sql = f"INSERT INTO facts (id_pi, id_date, max, min, avg, packets_sent, packets_received, packet_loss, hour) VALUES ('{data[0]}', {data[1]}, {data[2]}, {data[3]}, {data[4]}, '{data[5]}', '{data[6]}', '{data[7]}', '{data[8]}')"
                 cursor.execute(sql, data)
             time = hour_now()
             delete_last_updated(cursor)
