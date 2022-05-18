c1e0c642a52d59fa52817210cc871e2511d60689
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-05-18 10:45:14 +0100

    adding devices homepage basic layout

--- monitor_website/src/pages/devices/Devices.jsx
+++ monitor_website/src/pages/devices/Devices.jsx
@@ -1,10 +1,55 @@
-import "./devices.css";
-import React from "react";
+import "./devices.css"
+import React, { useState, useEffect } from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 
 const Devices = () => {
+    const [data, setData]  = useState([]);
+
+    const getData = async () => {
+        try {
+            const response = await fetch("/data/devices");
+            const jsonData = await response.json();
+
+            setData(jsonData);
+        }  catch (err) {
+            console.error(err);
+        }
+    };
+
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getDate = (params) => {
+        let day = params.row.day;
+        let month = parseInt(params.row.month) - 1; // in the Date format, month uses index, so we need to subtract 1
+        let year = params.row.year;
+        return (
+            new Date(year, month, day)
+        );
+    };
+
+    const columns = [
+        {field: 'id_pi', headerName: 'ID', headerAlign: 'center', width: 100},
+        {field: 'ip', headerName: 'Location', headerAlign: 'center', width: 100},
+        {field: 'model', headerName: 'Model', headerAlign: 'center', width: 80},
+        {field: 'location', headerName: 'Location', headerAlign: 'center', width: 80},
+        {field: 'date', headerName: 'Date', type: 'date', headerAlign: 'center', width: 120, valueGetter: getDate},
+        {field: 'hour', headerName: 'Time', type: 'time', headerAlign: 'center', width: 120},
+    ];
+
+
+
     return (
         <div className="devices">
-            <h2 className="title">Devices List</h2>
+            <DataGrid
+                rows={data}
+                columns={columns}
+                id="_id"
+                components={{Toolbar: GridToolbar}}
+                getRowId={(row) => (row.id_pi, row.ip) }
+                />
         </div>
     );
 };
