bf72e6665971ee8ad0441ab0b0050b78d0cf3b73
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-05-22 19:41:05 +0100

    updating .bash script to create the database

--- database/createdb.bash
+++ database/createdb.bash
@@ -3,6 +3,6 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 
 createdb testdb -O $USER
-psql -d testdb -f schema.sql
-/home/pi/Repos/networking-monitoring-raspberry/.venv/bin/python /home/pi/Repos/networking-monitoring-raspberry/client_scripts/register.py
+psql -d testdb -f schema.sql -f view.sql
+python /home/pi/Repos/network-monitoring-raspberry/client_scripts/register.py
 echo -e "${GREEN}Completed."
