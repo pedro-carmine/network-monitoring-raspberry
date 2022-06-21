d07f30d58ccb91117e6983b79829d866a3ac1f79
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-06-21 18:48:41 +0100

    adding visual connection status indicator for the webpage

--- monitor_website/src/pages/home/Home.jsx
+++ monitor_website/src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
 import "./home.css"
 import React, { useState, useEffect } from "react";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { Chip } from "@mui/material";
 import version from "../../version.js";
+import { green, red, amber } from "@mui/material/colors";
+import WarningIcon from "@material-ui/icons/Warning";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import CancelIcon from '@mui/icons-material/Cancel';
 
 const Home = () => {
     const [data, setData]  = useState([]);
@@ -31,6 +36,41 @@ const Home = () => {
         );
     };
 
+    function getChipProps(params) {
+        if (params.value === 'connected') {
+            return {
+                icon: <CheckCircleIcon style={{ fill: green[500] }} />,
+                label: 'Connected',
+                style: {
+                    borderColor: green[500]
+                }
+            };
+        }
+        else if (params.value === 'no_connection') {
+            return {
+                icon: <WarningIcon style={{ fill: amber[500] }} />,
+                label: 'No Connection',
+                style: {
+                    borderColor: amber[500]
+                }
+            };
+        }
+        else if (params.value === 'disconnected') {
+            return {
+                icon: <CancelIcon style={{ fill: red[500] }} />,
+                label: 'Disconnected',
+                style: {
+                    borderColor: red[500]
+                }
+            };
+        }
+        else {
+            return {
+                label: 'not defined'
+            }
+        }
+    };
+
     const columns = [
         {field: 'id_pi', headerName: 'Hostname', headerAlign: 'center', width: 100},
         {field: 'location', headerName: 'Location', headerAlign: 'center', width: 100},
@@ -49,10 +89,12 @@ const Home = () => {
                 return `${params.value} %`;
             }
         },
-        {field: 'connection_status', headerName: 'Status', headerAlign: 'center', width: 120},
         {field: 'date', headerName: 'Date', type: 'date', headerAlign: 'center', width: 120, valueGetter: getDate},
         {field: 'hour', headerName: 'Time', headerAlign: 'center', width: 120},
         {field: 'ip', headerName: 'IP Address', headerAlign: 'center', width: 100},
+        {field: 'connection_status', headerName: 'Connection Status', headerAlign: 'center', width: 170, renderCell: (params) => {
+            return <Chip variant="outlined" {... getChipProps(params)}/>
+        }}
     ];
 
 
