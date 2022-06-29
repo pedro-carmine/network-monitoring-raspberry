62d0ca71ba331fa23e599bdc98c41c9b0c3a5661
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-06-30 00:13:57 +0100

    adding ip update to the main database

--- client_scripts/check_ip.py
+++ client_scripts/check_ip.py
@@ -10,10 +10,20 @@ def get_ip_address():
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
 
-def update_ip(cursor, ip):
+def update_ip(local_cursor, ip):
     print(f"IP changed to: {ip}")
     sql = f"UPDATE raspberry SET ip = '{ip}' WHERE id_pi = '{hostname}'"
+    local_cursor.execute(sql)
+
+    # it also updates the main database
+    # if any error occurs, the commit is
+    # aborted in the local and main database
+    connection = psycopg2.connect(login)
+    cursor = connection.cursor()
     cursor.execute(sql)
+    connection.commit()
+    cursor.close()
+    connection.close()
 
 try:
     current_ip = get_ip_address()
