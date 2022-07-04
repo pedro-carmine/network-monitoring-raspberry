d21c24576bd6441b598d34cef88d365fd073811a
Pedro Carmine <pedro.carmine@tecnico.ulisboa.pt>
2022-07-04 13:53:01 +0100

    adding destination_ping to database/python scripts

--- client_scripts/monitor.py
+++ client_scripts/monitor.py
@@ -23,6 +23,21 @@ def get_time():
     ms = now.microsecond
     return f"{hour}:{minutes}:{seconds}.{ms}"
 
+def get_ping_destination(id_pi):
+    try:
+        connection = psycopg2.connect(f"dbname={LOCAL_DB_NAME} user={user}")
+        cursor = connection.cursor()
+        query = f"SELECT destination_ping FROM raspberry WHERE id_pi = '{id_pi}'"
+        cursor.execute(query)
+        result = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        
+        return result[0]
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        exit(1)
+
 id_pi = socket.gethostname()
 user = getpass.getuser()
 status = None
@@ -31,7 +46,7 @@ time = get_time()
 
 try:
     gateways = netifaces.gateways()
-    ping_destination = gateways['default'][netifaces.AF_INET][0]
+    ping_destination = get_ping_destination(id_pi)
     interface = gateways['default'][netifaces.AF_INET][1]
     
     transmitter.destination = ping_destination
@@ -56,8 +71,7 @@ try:
     if status == NO_CONNECTION and active_interfaces_count == 2: # try pinging with the wireless interface
         print(f"Interface {interface} with no connection, trying with another available interface")
         interface = gateways[2][1][1]
-        ping_destination = gateways[2][1][0]
-        print(interface, ping_destination)
+        print(f"Trying again with interface {interface}")
         
         transmitter.interface = interface
         transmitter.destination = ping_destination
--- client_scripts/register.py
+++ client_scripts/register.py
@@ -1,23 +1,43 @@
 from ast import Try
+import netifaces
 import socket
 import getpass
 import psycopg2
 from constants import LOCAL_DB_NAME
+
+def ask_for_input():
+    return input("Do you want to specify the destination ping?\nyes or no?\nIf the answer is no, the ping destination will be the default gateway\nAnswer: ")
 
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
 
-hostname = socket.gethostname()
-user = getpass.getuser()
-model = input("Raspberry Pi model: ")
-location = input("Raspberry Pi location: ")
+def validate_ping_destination(specify_destination):
+    if (specify_destination.lower() == 'yes'):
+        return input("Please insert the address of the destination ping: ").lower()
+    elif (specify_destination.lower() == 'no'):
+        gateways = netifaces.gateways()
+        return gateways['default'][2][0]
+    else:
+        print("Invalid validation, exiting")
+        exit(1)
 
 try:
     ip = get_ip_address()
 except Exception as e:
     print(f"Check if the device is connected to a network and has an IP address\nError: {e}")
+hostname = socket.gethostname()
+user = getpass.getuser()
+model = input("Raspberry Pi model: ")
+location = input("Raspberry Pi location: ")
+specify_destination = ask_for_input().lower()
+
+while specify_destination != 'yes' and specify_destination != 'no':
+    specify_destination = input("Invalid input, please answer yes or no\nAnswer: ").lower()
+
+destination_ping = validate_ping_destination(specify_destination)
+print(destination_ping)
 
 connection = None
 
@@ -25,7 +45,7 @@ try:
     connection = psycopg2.connect(f"dbname={LOCAL_DB_NAME} user={user}")
     cursor = connection.cursor()
     data = (hostname, model)
-    sql = f"INSERT INTO raspberry (id_pi, model, location, ip) VALUES ('{hostname}', '{model}', '{location}', '{ip}')"
+    sql = f"INSERT INTO raspberry (id_pi, model, location, ip, destination_ping) VALUES ('{hostname}', '{model}', '{location}', '{ip}', '{destination_ping}')"
     cursor.execute(sql, data)
     connection.commit()
     print(f"Successfully registered with id: {hostname}")
